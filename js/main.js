30b46d01d27fd57125bb5bc8f8dad25c3a7d7580
Nikita <nikulinn3349@gmail.com>
2022-03-20 20:04:57 +0300

    non-qowepo

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+  $('.slider__inner, .news__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false
+  });
+
+
+});
